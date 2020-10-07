b402746199731603aeafd4635b2ed1520d9de750
blvck <brianmauriceoluoch@gmail.com>
2020-10-07 21:51:57 +0300

    Deploy site

--- src/actions/baseUrl.js
+++ src/actions/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:8000';
-// const baseUrl = 'https://portfolio-backend-v1.herokuapp.com';
+// const baseUrl = 'http://localhost:8000';
+const baseUrl = "https://portfolio-backend-v1.herokuapp.com";
 
 export default baseUrl;

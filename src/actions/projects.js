1ffba35fe067ebb6439831e355b70ce728316de6
blvck <brianmauriceoluoch@gmail.com>
2020-10-08 12:30:54 +0300

    Updated data

--- src/actions/projects.js
+++ src/actions/projects.js
@@ -2,30 +2,35 @@ import axios from "axios";
 import {
   FETCH_PROJECTS_SUCCESS,
   FETCH_PROJECTS_ERROR,
-  FETCH_PROJECTS_LOADING
+  FETCH_PROJECTS_LOADING,
 } from "./types";
-import baseUrl from './baseUrl'
+import baseUrl from "./baseUrl";
+import projects from "../data";
 
 export const fetchProjects = () => (dispatch) => {
-
-  dispatch({
-    type: FETCH_PROJECTS_LOADING,
-  });
+  console.log(projects);
   
-  axios
-    .get(`${baseUrl}/api/projects`)
-    .then((res) => {
-      dispatch({
-        type: FETCH_PROJECTS_SUCCESS,
-        payload: res.data,
-      });
-    })
-    .catch((err) => {
-      dispatch({
-        type: FETCH_PROJECTS_ERROR,
-        payload: err.response
-          ? err.response.statusText
-          : "Could not connect to Internet",
-      });
-    });
+  dispatch({type: FETCH_PROJECTS_LOADING })
+
+  if(projects.length > 0){
+    dispatch({
+    type: FETCH_PROJECTS_SUCCESS,
+    payload: projects,
+  });
+  }
+
+  
+
+  
+
+  // .catch((err) => {
+    
+  // });
+
+  // axios
+  //   .get(`${baseUrl}/api/projects`)
+  //   .then((res) => {
+      
+  //   })
+    
 };

42fde6405db1ba3cf25627dcf7f68eb82215276b
blvck <brianmauriceoluoch@gmail.com>
2020-10-23 12:53:14 +0300

    Updated width

--- src/component/common/LeftSide.js
+++ src/component/common/LeftSide.js
@@ -8,37 +8,28 @@ function LeftSide() {
           <a
             href="https://github.com/blvck-code"
             target="_blank"
-            className="fa fa-github"
-          ></a>
-          <span>github/blvck-code</span>
+            className="fa fa-github"></a>
         </div>
         <div>
           <a
             href="https://www.linkedin.com/in/maurice-brian-oluoch-ba1269121/"
             target="_blank"
-            className="fa fa-linkedin"
-          ></a>
-          <span>linkedin/maurice-brian-oluoch</span>
+            className="fa fa-linkedin"></a>
         </div>
         <div>
           <a href="tel:+254795772333" className="fa fa-phone"></a>
-          <span>+254795772333</span>
         </div>
         <div>
           <a
             href="https://www.instagram.com/oluoch_jnr/"
             target="_blank"
-            className="fa fa-instagram"
-          ></a>
-          <span>instagram/oluoch_jnr</span>
+            className="fa fa-instagram"></a>
         </div>
         <div>
           <a
             href="https://twitter.com/blvck_code"
             target="_blank"
-            className="fa fa-twitter"
-          ></a>
-          <span>twitter/blvck_code</span>
+            className="fa fa-twitter"></a>
         </div>
       </div>
       <div className="line"></div>
